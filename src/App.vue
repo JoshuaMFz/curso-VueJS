7bb35579093d654cbddb0da811b08d8751630f65
Joshua Martins Fernandes <joshuak6k@gmail.com>
2024-03-10 21:01:55 -0400

    Diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <template>
-  <TheHeader v-show="showHeader" />
-  <div v-show="showName">
-    Nome: {{firstname}} <br>
-    Sobrenome: {{lastname}}
+  <div>
+
+
+
+
+    <div v-for="obj in todos" v-bind:key="obj.id" class="todos-item">
+      {{obj.title}}
+    </div>
+
+
+
   </div>
-  <div v-if="acessLevel === 'admin'">Admin</div>
-  <div v-else-if="acessLevel === 'marketing'">Marketing</div>
-  <div v-else>User</div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
-  import TheHeader from './components/TheHeader.vue'
-
   export default {
     name: 'App',
     components: {
-      HelloWorld, TheHeader
     }, data() {
       return {
-        showHeader: false,
-        firstname: "Jon",
-        lastname: "Snow",
-        showName: false,
-        acessLevel: "admin"
+        todos: [
+          {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+          }
+        ]
       }
     }
   }
@@ -36,8 +61,16 @@
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    color: #000;
+    margin-left: 60px;
     margin-top: 60px;
+  }
+
+  .todos-item {
+    background-color: #000;
+    color: #fff;
+    padding: 3px 5px;
+    margin-bottom: 5px;
+    max-width: 80%;
   }
 </style>
