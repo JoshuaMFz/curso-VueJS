c0f1f2bac06dbefb5084aa26a85ecd736adaabe8
Joshua Martins Fernandes <joshuak6k@gmail.com>
2024-03-10 20:37:40 -0400

    Início de Curso - Diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
+  <TheHeader v-show="showHeader" />
+  <div v-show="showName">
+    Nome: {{firstname}} <br>
+    Sobrenome: {{lastname}}
+  </div>
+  <div v-if="acessLevel === 'admin'">Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import TheHeader from './components/TheHeader.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld, TheHeader
+    }, data() {
+      return {
+        showHeader: false,
+        firstname: "Jon",
+        lastname: "Snow",
+        showName: false,
+        acessLevel: "admin"
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
